691e2680fbbbc07bd99ff3179c4898fb54d73fe6
kryz <k@kryz.dev>
2020-12-25 21:36:51 +0100

    initial config

---
+++ src/Example.java
@@ -0,0 +1,11 @@
+public class Example {
+    private int number = 1;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(final int number) {
+        this.number = number;
+    }
+}
